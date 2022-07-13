b54112ffd6a98ea952434b83d5f71ab73ec49329
Adam DeCaluwe <adecaluwe@d127.org>
2022-07-13 13:45:26 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Adam DeCaluwe on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
